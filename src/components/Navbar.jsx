b3a936bed8f0de277ee6e48579274b3ecfc695fe
Izyan Fairuz <izyanfairuz25@gmail.com>
2025-12-30 17:22:53 +0000

    Fix Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
-        <a className="navbar-brand" href="#">NexusSoft</a>
+        <a className="navbar-brand" href="/">NexusSoft</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
           <span className="navbar-toggler-icon"></span>
         </button>
